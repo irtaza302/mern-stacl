49a2f3ca9322e95dc863bd429c12446558e74d9f
Irtaza malik <malikirtaza96@gmail.com>
2024-09-16 12:31:44 +0000

    first commint

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
